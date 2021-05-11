16dfc527e79b8fbf8c4c67cc721a1a708895c395
Rahul <pacharne.rahul92@gmail.com>
2021-05-11 09:32:35 +0530

    changes for followers and pagination

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['pacharne.rahul92@gmail.com']
+    POSTS_PER_PAGE = 10
